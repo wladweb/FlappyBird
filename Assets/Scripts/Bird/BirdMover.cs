2022f7f1eeca3920068ffa23524b16c8ec9ddd9b
wladweb <wladwebwork@gmail.com>
2020-05-09 21:16:13 +0300

    add bird moving

---
+++ Assets/Scripts/Bird/BirdMover.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BirdMover : MonoBehaviour
+{
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _speed;
+    [SerializeField] private Vector3 _startPosition;
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _maxRotationZ;
+    [SerializeField] private float _minRotationZ;
+    
+    private Rigidbody2D _rigidBody;
+    private Quaternion _maxRotation;
+    private Quaternion _minRotation;
+
+    private void Start()
+    {
+        transform.position = _startPosition;
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _rigidBody.velocity = Vector2.zero;
+        _maxRotation = Quaternion.Euler(0, 0, _maxRotationZ);
+        _minRotation = Quaternion.Euler(0, 0, _minRotationZ);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidBody.velocity = new Vector2(_speed, 0);
+            transform.rotation = _maxRotation;
+            _rigidBody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, _minRotation, _rotationSpeed * Time.deltaTime);
+    }
+}
