67de39778f8857d846a22f49fac6cc72187e0121
wladweb <wladwebwork@gmail.com>
2020-05-10 17:05:51 +0300

    add score displayer

--- Assets/Scripts/Bird/Bird.cs
+++ Assets/Scripts/Bird/Bird.cs
@@ -8,6 +8,7 @@ public class Bird : MonoBehaviour
     private int _score;
 
     public event UnityAction GameOver;
+    public event UnityAction<int> ScoreChanged;
 
     private void Start()
     {
@@ -17,12 +18,14 @@ public class Bird : MonoBehaviour
     public void ResetPlayer()
     {
         _score = 0;
+        ScoreChanged?.Invoke(_score);
         _mover.ResetBird();
     }
 
     public void IncreaseScore()
     {
         _score++;
+        ScoreChanged?.Invoke(_score);
     }
 
     public void Die()
